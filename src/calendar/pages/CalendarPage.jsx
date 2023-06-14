aa76be86e4492aeb2c2d6fc62c5e0f1d2b293376
juan barrios <juanpablobarriosgrijalba@gmail.com>
2023-06-14 12:43:59 -0500

    react big calendar 349

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,5 +1,37 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, format, parse, startOfWeek, getDay} from "date-fns";
+import enUS from 'date-fns/locale/en-US'
 import { Navbar } from "../"
 
+
+const locales = {
+  'en-US': enUS,
+}
+
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+
+const events = [{
+
+title: "cumpleaños del jefe",
+note: " hay que comprar el patel ",
+start : "ne day()",
+end: addHours (new Date (),2 ),
+bgcolor: "#fafafa",
+user:{
+_id:"123",
+name: "juan pablo"
+}
+
+}]
 
 export const CalendarPage = () => {
   return (
@@ -7,6 +39,16 @@ export const CalendarPage = () => {
 <>
 
 <Navbar/>
+<Calendar
+      localizer={ localizer }
+      events={ events }
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: "calc ( 100vh - 80px)" }}
+    />
+
+
+
 </>
 
   )
