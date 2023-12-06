ee18611411acaae3b8834eb5a86c92eaacb71f5f
juan barrios <juanpablobarriosgrijalba@gmail.com>
2023-12-06 12:11:10 -0500

    CrearNuevoEvento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -5,21 +5,26 @@ import {
   onSetActiveEvent,
   onUpdateEvent,
 } from "../store/";
+import { calendarApi } from "../api";
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
   const { events, activeEvent } = useSelector((state) => state.calendar);
+  const { user } = useSelector((state) => state.auth);
 
   const setActiveEvent = (calendarEvent) => {
     dispatch(onSetActiveEvent(calendarEvent));
   };
 
   const startSavingEvent = async (calendarEvent) => {
+    //TODO: uptade Event
     if (calendarEvent._id) {
       dispatch(onUpdateEvent({ ...calendarEvent }));
     } else {
+      const { data } = await calendarApi.post("/events", calendarEvent);
+      console.log({ data });
       dispatch(
-        onAddActiveEvent({ ...calendarEvent, _id: new Date().getTime() })
+        onAddActiveEvent({ ...calendarEvent, id: data.evento.id, user })
       );
     }
   };
